f79d21214794bd3bebe261cb492b5c78018f4e0f
kyzwtf <85465074+kyzwtf@users.noreply.github.com>
2021-06-07 03:58:57 -0400

    Create Rawiri-X.lua

---
+++ Rawiri-X.lua
@@ -0,0 +1,17 @@
+local player = game.Players.LocalPlayer
+local whitelistedpeople = {
+    "0owah"
+    "Ruitalix"
+    "tezownsyou"
+    "Negations"
+    "TBANDOZZ"
+    "zenthys"
+    "77777MORTAL"
+    "Provatel
+  
+}
+for i,v in pairs (whitelistedpeople) do
+    if player.Name == v then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/kyzwtf/Rawiri/main/Rawiri",true))();
+    end
+end
